8d58fe5eb781223bb36bdee0a64a44b3f6627c80
Adib Chisty <adibc@Adibs-MacBook-Pro.local>
2019-03-20 17:20:39 -0400

    Update

--- Robot_Code/main/main.ino
+++ Robot_Code/main/main.ino
@@ -6,8 +6,30 @@
 //======== INCLUSIONS =======
 //===========================
 //========PIN DEFINITIONS====
+int R_MOTORSPEED_PIN = 6;
+int R_MOTORDIR_PIN = 7;
+int L_MOTORSPEED_PIN = 5;
+int L_MOTORDIR_PIN = 4;
+
+int IR_R_SENSORPIN = 1;
+int IR_M_SENSORPIN = 0;
+int IR_L_SENSORPIN = 2;
+
+int FRONT_DISTANCE_SENSORPIN = 3;
+
+int R_BUMPERPIN = 13;
+int L_BUMPERPIN = 10;
+
+int PAN_SERVO_PIN = 3;
+int TILT_SERVO_PIN = 8;
+int GRIPPER_PIN = 9;
+int GRIPPER_SENSOR_PIN = A2;
+
+int START_BUTTONPIN = 3;
 //===========================
 //========MISC DEFINITIONS===
+//===========================
+//========THRESHOLDS=========
 //===========================
 
 
@@ -20,3 +42,9 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+//=====MOVEMENT FUNCTIONS====
+//===========================
+
+//=====SERVO FUNCTIONS=======
+//===========================
