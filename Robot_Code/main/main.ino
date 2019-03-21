0ee859775538b6180b88400cbd6fe2f70cb4386b
Kitorus <bkitor@gmail.com>
2019-03-20 21:34:18 -0400

    Wrote Grab and Drop, added more fields to Robot

--- Robot_Code/main/main.ino
+++ Robot_Code/main/main.ino
@@ -1,3 +1,9 @@
+
+$ //IR SENSOR BOTTOM
+
+
+bkito@BENK-PC MINGW64 ~/Documents/Projects/ELEC299Bot
+$ int IR_R_SENSORPIN = 1;
 /*Program describing behaviour of warehouse robot prototype
  * Created by Adib Chisty on March 20, 2019
  * For ELEC299
@@ -56,15 +62,19 @@ class Robot {
     int L_MotorSpeed = 250;
     int R_SlowMotorSpeed = 100;
     int L_SlowMotorSpeed = 100;
-    
+
     int IR_Right_Thresh = 950;
     int IR_Middle_Thresh = 900;
     int IR_Left_Thresh = 950;
 
+    int Gripper_Pressure_Threshold = ;//int for comparing grip.analogRead(GRIPPER_SENSOR_PIN) used in grab()
+    int Ball_Pick_Up_Angle = ;//angle for tilt servo when picking up ball, used in grab()
+    int Ball_Drop_Off_Angle = ;//angle for tilt servo when droping of ball, used in drop()
+
     int Gripper_Thresh = 1000;
     int Gripper_Start_Position = 40;
     int Pan_Default_Position = 0;
-    int Tilt_Grab_Position = 180;
+    int Tilt_Grab_Position = 160;
     int Tilt_Up_Position = 0;
 
     int Grab_Distance_Thresh = 500;
@@ -76,9 +86,9 @@ class Robot {
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  
+
   Robot robot = Robot();
-  
+
   if (Serial.available() == 1) {
     Serial.write("ready");
     char iByte = Serial.read();
@@ -93,9 +103,9 @@ void setup() {
        int robotId = 3;
     }
   }
-  
+
   int robotId = 1; //or 2 or 3. This number should be a return value from the detectBeacon Function
-  
+
   pinMode(R_MOTORDIR_PIN, OUTPUT);
   pinMode(L_MOTORDIR_PIN, OUTPUT);
   pinMode(R_MOTORSPEED_PIN , OUTPUT);
@@ -106,22 +116,22 @@ void setup() {
 
   pan.attach(PAN_SERVO_PIN);
   tilt.attach(TILT_SERVO_PIN);
-  grip.attach(GRIPPER_PIN); 
+  grip.attach(GRIPPER_PIN);
   pinMode(GRIPPER_SENSOR_PIN, INPUT);
 
   pinMode(FRONT_DISTANCE_SENSORPIN,INPUT);
 
   pinMode(START_BUTTONPIN, INPUT);
-  
+
   pan.write(robot.Pan_Default_Position);
   tilt.write(robot.Tilt_Up_Position);
   grip.write(robot.Gripper_Start_Position);
-  
-  
-  
-  
+
+
+
+
   //Starting Sequence vvvvvvv
-  
+
   int val = digitalRead(START_BUTTONPIN);
   while ( val == HIGH){
       val = digitalRead(START_BUTTONPIN);
@@ -144,7 +154,7 @@ void loop() {
 
 
 void forwards(Robot R){
-  //TO DO 
+  //TO DO
 }
 
 void backwards(Robot R){
@@ -163,12 +173,24 @@ void approach(Robot R){
 
 //=====SERVO FUNCTIONS=======
 
+//angles tilt server over the ball
+//closes gripper comparing untill pressure pin tells it to stop
+//moves tilt into a vertical position
 void grab(Robot R){
   //TO DO
+  tilt.write(R.Ball_Pick_Up_Angle);//Find int for proper angle for pick up
+  grip.write(50);
+  for(int gripperDistance = 50;analogRead(GRIPPER_SENSOR_PIN)>R.Gripper_Pressure_Threshold; gripperDistance+=2){
+    grip.write(gripperDistance);
+  }
+  tilt.write(160);
 }
 
 void drop(Robot R){
   //TO DO
+  tilt.write(R.Ball_Drop_Off_Angle);
+  grip.write(40);
+  tilt.write(160);
 }
 
 //===========================
