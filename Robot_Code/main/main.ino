567fa73f61e80cda2bac3a8eb58a4b453a47b6f3
Adib Chisty <adibc@Adibs-MacBook-Pro.local>
2019-03-22 16:31:21 -0400

    Changed to Coord System

--- Robot_Code/main/main.ino
+++ Robot_Code/main/main.ino
@@ -1,11 +1,9 @@
 
-$ //IR SENSOR BOTTOM
 
 
-bkito@BENK-PC MINGW64 ~/Documents/Projects/ELEC299Bot
-$ int IR_R_SENSORPIN = 1;
 /*Program describing behaviour of warehouse robot prototype
  * Created by Adib Chisty on March 20, 2019
+ * Inspired by Husnain Haider's Coordinate System
  * For ELEC299
  */
 
@@ -17,46 +15,73 @@ QSerial myIRserial;
 
 //========PIN DEFINITIONS====
 //MOTORS
-int R_MOTORSPEED_PIN = 6;
-int R_MOTORDIR_PIN = 7;
-int L_MOTORSPEED_PIN = 5;
-int L_MOTORDIR_PIN = 4;
-
+int R_Dir= 7;       //M1
+int R_Speed = 6;    //E1
+int L_Speed = 5;    //E2
+int L_Dir = 4;      //M2
+  
 //IR SENSOR BOTTOM
-int IR_R_SENSORPIN = 1;
-int IR_M_SENSORPIN = 0;
-int IR_L_SENSORPIN = 2;
+int R_IR = A5;
+int M_IR = A4;
+int L_IR = A3;
 
 //IR SENSOR FRONT
-int FRONT_DISTANCE_SENSORPIN = 3;
+int F_IR = A2;
 
 //BUMPERS
-int R_BUMPERPIN = 13;
-int L_BUMPERPIN = 10;
+int R_Bumper = 3;
+int L_Bumper = 12;
 
 //SERVOS
-int PAN_SERVO_PIN = 3;
-int TILT_SERVO_PIN = 8;
-int GRIPPER_PIN = 9;
-int GRIPPER_SENSOR_PIN = A2;
+int pan_PIN = 8;
+int tilt_PIN = 9;
+int grip_PIN = 10;
+int pressure_PIN = A0;
+
 Servo pan, tilt, grip;
 
 //MISC
-int START_BUTTONPIN = 3;
-#define rxpin 4
+int start_PIN = 11;
+
+/*
+#define rxpin 4      IDK what this is for
 #define txpin -1
+*/
 
 //===========================
 
 //========MISC DEFINITIONS===
 
+//cordinate and cardinality system
+int cy = 0;
+int cx = 0;
+int cd = 0; //direction 0 = 'NORTH' 1,2,3 = E,S,W
+
+//integers to hold IR sensor analog values
+//bottom facing
+int lVal = 0;
+int cVal = 0;
+int rVal = 0;
+
+//previous value holders
+int plVal = 0;
+int pcVal = 0;
+int prVal = 0;
+
+//front facing
+int IRVal = 0;
+
+//timer thing
+int lastInter = 0;
+
 //===========================
 
 //========THRESHOLDS=========
+int thresh = 750;
 //===========================
 
-//========CLASSES============
-class Robot {
+//========CLASSES============ //No need for using a class, move thresholds into definition area
+class Robot { 
   public:
     int R_MotorSpeed = 250;
     int L_MotorSpeed = 250;
@@ -67,9 +92,9 @@ class Robot {
     int IR_Middle_Thresh = 900;
     int IR_Left_Thresh = 950;
 
-    int Gripper_Pressure_Threshold = ;//int for comparing grip.analogRead(GRIPPER_SENSOR_PIN) used in grab()
-    int Ball_Pick_Up_Angle = ;//angle for tilt servo when picking up ball, used in grab()
-    int Ball_Drop_Off_Angle = ;//angle for tilt servo when droping of ball, used in drop()
+    int Gripper_Pressure_Threshold = 0;//int for comparing grip.analogRead(GRIPPER_SENSOR_PIN) used in grab()
+    int Ball_Pick_Up_Angle = 0;//angle for tilt servo when picking up ball, used in grab()
+    int Ball_Drop_Off_Angle = 0;//angle for tilt servo when droping of ball, used in drop()
 
     int Gripper_Thresh = 1000;
     int Gripper_Start_Position = 40;
@@ -89,6 +114,9 @@ void setup() {
 
   Robot robot = Robot();
 
+  //=================== IR BEACON detection goes here
+/*
+
   if (Serial.available() == 1) {
     Serial.write("ready");
     char iByte = Serial.read();
@@ -103,25 +131,30 @@ void setup() {
        int robotId = 3;
     }
   }
+*/
+//=====================================================
 
   int robotId = 1; //or 2 or 3. This number should be a return value from the detectBeacon Function
 
-  pinMode(R_MOTORDIR_PIN, OUTPUT);
-  pinMode(L_MOTORDIR_PIN, OUTPUT);
-  pinMode(R_MOTORSPEED_PIN , OUTPUT);
-  pinMode(L_MOTORSPEED_PIN, OUTPUT);
+  pinMode(R_Dir, OUTPUT);
+  pinMode(L_Dir, OUTPUT);
+  pinMode(R_Speed , OUTPUT);
+  pinMode(L_Speed, OUTPUT);
 
-  pinMode(R_BUMPERPIN, INPUT);
-  pinMode(L_BUMPERPIN, INPUT);
+  pinMode(R_Bumper, INPUT);
+  pinMode(L_Bumper, INPUT);
 
-  pan.attach(PAN_SERVO_PIN);
-  tilt.attach(TILT_SERVO_PIN);
-  grip.attach(GRIPPER_PIN);
-  pinMode(GRIPPER_SENSOR_PIN, INPUT);
+  pan.attach(pan_PIN);
+  tilt.attach(tilt_PIN);
+  grip.attach(grip_PIN);
+  pinMode(pressure_PIN, INPUT);
 
-  pinMode(FRONT_DISTANCE_SENSORPIN,INPUT);
+  pinMode(F_IR,INPUT);
+  pinMode(L_IR,INPUT);
+  pinMode(M_IR,INPUT);
+  pinMode(R_IR,INPUT);
 
-  pinMode(START_BUTTONPIN, INPUT);
+  pinMode(start_PIN, INPUT);
 
   pan.write(robot.Pan_Default_Position);
   tilt.write(robot.Tilt_Up_Position);
@@ -132,12 +165,12 @@ void setup() {
 
   //Starting Sequence vvvvvvv
 
-  int val = digitalRead(START_BUTTONPIN);
+  int val = digitalRead(start_PIN);
   while ( val == HIGH){
-      val = digitalRead(START_BUTTONPIN);
+      val = digitalRead(start_PIN);
       if (val == LOW) {
         while (val == LOW){
-          val = digitalRead(START_BUTTONPIN);
+          val = digitalRead(start_PIN);
         }
         break;
       }
@@ -151,13 +184,15 @@ void loop() {
 
 //=====MOVEMENT FUNCTIONS====
 
-
-
-void forwards(Robot R){
+void goToCoord(Robot R,int x, int y, int d){
   //TO DO
 }
 
-void backwards(Robot R){
+void forward(Robot R, int numOfIntersections){
+  //TO DO
+}
+
+void backward(Robot R){ //kinda redundant because no sensors on the back, will REMOVE
   //TO DO
 }
 
@@ -166,6 +201,10 @@ void turn(Robot R, int dir){
 }
 
 void approach(Robot R){
+  //TO DO
+}
+
+void correctCourse(Robot R){
   //TO DO
 }
 
@@ -180,7 +219,7 @@ void grab(Robot R){
   //TO DO
   tilt.write(R.Ball_Pick_Up_Angle);//Find int for proper angle for pick up
   grip.write(50);
-  for(int gripperDistance = 50;analogRead(GRIPPER_SENSOR_PIN)>R.Gripper_Pressure_Threshold; gripperDistance+=2){
+  for(int gripperDistance = 50;analogRead(pressure_PIN)>R.Gripper_Pressure_Threshold; gripperDistance+=2){
     grip.write(gripperDistance);
   }
   tilt.write(160);
