154e46c09e2b8a37e82892a071a4684791507bfc
skipperadib <skipperadib@gmail.com>
2019-03-23 16:17:42 -0400

    Code Formatting...

--- Robot_Code/main/main.ino
+++ Robot_Code/main/main.ino
@@ -2,12 +2,12 @@
 
 
 /*Program describing behaviour of warehouse robot prototype
- * Created by Adib Chisty on March 20, 2019
- * Inspired by Husnain Haider's Coordinate System
- * Expanded with Raed Fayad's CLass
- * Modularity added
- * For ELEC299
- */
+* Created by Adib Chisty on March 20, 2019
+* Inspired by Husnain Haider's Coordinate System
+* Expanded with Raed Fayad's CLass
+* Modularity added
+* For ELEC299
+*/
 
 //======== INCLUSIONS =======
 #include <Servo.h>
@@ -84,29 +84,29 @@ int thresh = 750;
 
 //========CLASSES============ //
 class Robot {
-  public:
-    int R_FastMotorSpeed = 250;
-    int L_FastMotorSpeed = 250;
-    int R_MedMotorSpeed = 150;
-    int L_MedMotorSpeed = 150;
-    int R_SlowMotorSpeed = 100;
-    int L_SlowMotorSpeed = 100;
+public:
+  int R_FastMotorSpeed = 250;
+  int L_FastMotorSpeed = 250;
+  int R_MedMotorSpeed = 150;
+  int L_MedMotorSpeed = 150;
+  int R_SlowMotorSpeed = 100;
+  int L_SlowMotorSpeed = 100;
 
-    int IR_Right_Thresh = 950;
-    int IR_Middle_Thresh = 900;
-    int IR_Left_Thresh = 950;
+  int IR_Right_Thresh = 950;
+  int IR_Middle_Thresh = 900;
+  int IR_Left_Thresh = 950;
 
-    int Gripper_Pressure_Threshold = 0;//int for comparing grip.analogRead(GRIPPER_SENSOR_PIN) used in grab()
-    int Ball_Pick_Up_Angle = 0;//angle for tilt servo when picking up ball, used in grab()
-    int Ball_Drop_Off_Angle = 0;//angle for tilt servo when droping of ball, used in drop()
+  int Gripper_Pressure_Threshold = 0;//int for comparing grip.analogRead(GRIPPER_SENSOR_PIN) used in grab()
+  int Ball_Pick_Up_Angle = 0;//angle for tilt servo when picking up ball, used in grab()
+  int Ball_Drop_Off_Angle = 0;//angle for tilt servo when droping of ball, used in drop()
 
-    int Gripper_Thresh = 1000;
-    int Gripper_Start_Position = 40;
-    int Pan_Default_Position = 0;
-    int Tilt_Grab_Position = 160;
-    int Tilt_Up_Position = 0;
+  int Gripper_Thresh = 1000;
+  int Gripper_Start_Position = 40;
+  int Pan_Default_Position = 0;
+  int Tilt_Grab_Position = 160;
+  int Tilt_Up_Position = 0;
 
-    int Grab_Distance_Thresh = 500;
+  int Grab_Distance_Thresh = 500;
 
 };
 //===========================
@@ -119,28 +119,28 @@ void setup() {
   Robot robot = Robot();
 
   //=================== IR BEACON detection goes here
-/*
+  /*
 
   if (Serial.available() == 1) {
-    Serial.write("ready");
-    char iByte = Serial.read();
-    Serial.println(iByte);
-    if (iByte == 'Y') {
-       int robotId = 1;
-    }
-    if (iByte == 'X') {
-       int robotId = 2;
-    }
-    if (iByte == 'Z') {
-       int robotId = 3;
-    }
-  }
+  Serial.write("ready");
+  char iByte = Serial.read();
+  Serial.println(iByte);
+  if (iByte == 'Y') {
+  int robotId = 1;
+}
+if (iByte == 'X') {
+int robotId = 2;
+}
+if (iByte == 'Z') {
+int robotId = 3;
+}
+}
 */
 //=====================================================
 
   int robotId = 1; //or 2 or 3. This number should be a return value from the detectBeacon Function
 
-  //PINMODE SETUPS
+//PINMODE SETUPS
   pinMode(R_Dir, OUTPUT);
   pinMode(L_Dir, OUTPUT);
   pinMode(R_Speed , OUTPUT);
@@ -168,17 +168,17 @@ void setup() {
 
 
 
-  //Starting Sequence vvvvvvv
+//Starting Sequence vvvvvvv
 
   int val = digitalRead(start_PIN);
   while ( val == HIGH){
-      val = digitalRead(start_PIN);
-      if (val == LOW) {
-        while (val == LOW){
-          val = digitalRead(start_PIN);
-        }
-        break;
+    val = digitalRead(start_PIN);
+    if (val == LOW) {
+      while (val == LOW){
+        val = digitalRead(start_PIN);
       }
+      break;
+    }
   }
 }
 
@@ -212,26 +212,26 @@ void goToCoord(Robot R,int x, int y, int d){ //x,y is destination coord, d is th
     }
   }
   if(cd!=d){ //if the robot is not facing the ball, then turn towards the ball
-      if(abs(cd-d) == 2){ //ball is 180deg behind robot, turn 90deg twice [Cases: Many]
-        turn(robot,1);
-        turn(robot,1);
-      }
-      else if(cd-d ==-3){ //ball is -90deg to robot, turn -90deg(Left) once [Case: facing 0, ball at 3]
-        turn (robot,1);
-      }
-      else if(cd-d ==3){  //ball is 90deg to robot, turn 90deg(Right) once  [Case: facing 3, ball at 0]
-        turn (robot,0);
-      }
-      //if didnt catch above cases, adjust direction
-      else if(cd-d>0)  //ball is -90deg to robot, turn -90deg(Left)   [Cases: Many]
-      {
-        turn(robot,1);
-      }
-      else if(cd-d<0) //ball is 90deg to robot, turn 90deg(Right)     [Cases: Many]
-      {
-        turn(robot,0);
-      }
+    if(abs(cd-d) == 2){ //ball is 180deg behind robot, turn 90deg twice [Cases: Many]
+      turn(robot,1);
+      turn(robot,1);
     }
+    else if(cd-d ==-3){ //ball is -90deg to robot, turn -90deg(Left) once [Case: facing 0, ball at 3]
+      turn (robot,1);
+    }
+    else if(cd-d ==3){  //ball is 90deg to robot, turn 90deg(Right) once  [Case: facing 3, ball at 0]
+      turn (robot,0);
+    }
+    //if didnt catch above cases, adjust direction
+    else if(cd-d>0)  //ball is -90deg to robot, turn -90deg(Left)   [Cases: Many]
+    {
+      turn(robot,1);
+    }
+    else if(cd-d<0) //ball is 90deg to robot, turn 90deg(Right)     [Cases: Many]
+    {
+      turn(robot,0);
+    }
+  }
 
 }
 
@@ -241,29 +241,29 @@ void forward(Robot R, int numOfIntersections){
   digitalWrite(R_Dir, HIGH); //Set right motor direction to forward
 
   //While not at the desired intersection, keep moving forward
- while(intersectionCount < numOfIntersections){
-   lVal = analogRead(L_IR);
-   mVal = analogRead(M_IR);
-   rVal = analogRead(R_IR);
+  while(intersectionCount < numOfIntersections){
+    lVal = analogRead(L_IR);
+    mVal = analogRead(M_IR);
+    rVal = analogRead(R_IR);
 
-   correctCourse(robot); //correct leaning
+    correctCourse(robot); //correct leaning
 
-  //correctly detect intersections, prevents accidental detection of false intersection
-   if((lVal > thresh) && (mVal > thresh) && (rVal > thresh) && (plVal > thresh) && (pmVal > thresh) && (prVal > thresh)){ //At an intersection. Increment intersection counter
-     if((millis()-lastInter)>200){
-       Serial.println("INTERSECTION DETECTED");
+    //correctly detect intersections, prevents accidental detection of false intersection
+    if((lVal > thresh) && (mVal > thresh) && (rVal > thresh) && (plVal > thresh) && (pmVal > thresh) && (prVal > thresh)){ //At an intersection. Increment intersection counter
+      if((millis()-lastInter)>200){
+        Serial.println("INTERSECTION DETECTED");
 
-       intersectionCount++;
-       Serial.println(intersectionCount);
-       lastInter = millis();
+        intersectionCount++;
+        Serial.println(intersectionCount);
+        lastInter = millis();
 
-     }
-   }
+      }
+    }
     plVal = lVal;
     pmVal = mVal;
     prVal = rVal;
- }
- stop(robot);
+  }
+  stop(robot);
 }
 
 
@@ -278,55 +278,55 @@ void stop(Robot R){
 
 void turn(Robot R, int dir){
   //drive a little past the intersection
- delay(50);
- analogWrite(L_Speed, R.L_SlowMotorSpeed);
- analogWrite(R_Speed, R.R_SlowMotorSpeed);
- digitalWrite(L_Dir, HIGH);
- digitalWrite(R_Dir, HIGH);
- delay(700);
+  delay(50);
+  analogWrite(L_Speed, R.L_SlowMotorSpeed);
+  analogWrite(R_Speed, R.R_SlowMotorSpeed);
+  digitalWrite(L_Dir, HIGH);
+  digitalWrite(R_Dir, HIGH);
+  delay(700);
 
- if(dir == 1){//Begin turning counter-clockwise to ensure that center line sensor is not scaning the black tape
-     analogWrite(L_Speed, R.L_SlowMotorSpeed);
-     analogWrite(R_Speed, R.R_SlowMotorSpeed);
-     digitalWrite(L_Dir, LOW);
-     digitalWrite(R_Dir, HIGH);
-     delay(200);
+  if(dir == 1){//Begin turning counter-clockwise to ensure that center line sensor is not scaning the black tape
+    analogWrite(L_Speed, R.L_SlowMotorSpeed);
+    analogWrite(R_Speed, R.R_SlowMotorSpeed);
+    digitalWrite(L_Dir, LOW);
+    digitalWrite(R_Dir, HIGH);
+    delay(200);
   }
   else{//Begin turning clockwise to ensure that center line sensor is not scaning the black tape
     analogWrite(L_Speed, R.L_SlowMotorSpeed);
-     analogWrite(R_Speed, R.R_SlowMotorSpeed);
-     digitalWrite(L_Dir, HIGH);
-     digitalWrite(R_Dir, LOW);
-     delay(200);
+    analogWrite(R_Speed, R.R_SlowMotorSpeed);
+    digitalWrite(L_Dir, HIGH);
+    digitalWrite(R_Dir, LOW);
+    delay(200);
   }
 
- mVal = analogRead(cIRPin);
- while(mVal<thresh){ //Rotate in specified direction until the middle line sensor reads the black tape value
-   mVal = analogRead(cIRPin);
-   analogWrite(L_Speed, R.L_SlowMotorSpeed);
-   analogWrite(R_Speed, R.R_SlowMotorSpeed);
-   if(dir = 1){
-     digitalWrite(L_Dir, LOW);
-     digitalWrite(R_Dir, HIGH);
-   }else{
-     digitalWrite(L_Dir, HIGH);
-     digitalWrite(R_Dir, LOW);
-   }
- }
+  mVal = analogRead(cIRPin);
+  while(mVal<thresh){ //Rotate in specified direction until the middle line sensor reads the black tape value
+    mVal = analogRead(cIRPin);
+    analogWrite(L_Speed, R.L_SlowMotorSpeed);
+    analogWrite(R_Speed, R.R_SlowMotorSpeed);
+    if(dir = 1){
+      digitalWrite(L_Dir, LOW);
+      digitalWrite(R_Dir, HIGH);
+    }else{
+      digitalWrite(L_Dir, HIGH);
+      digitalWrite(R_Dir, LOW);
+    }
+  }
 
- //Update current direction of the robot
- if(dir == 1){
-     if(cd ==0){ //Update direction robot is facing
+  //Update current direction of the robot
+  if(dir == 1){
+    if(cd ==0){ //Update direction robot is facing
       cd =3;
-     }else{
+    }else{
       cd =cd-1;
-     }
+    }
   }
   else{
-     cd = (cd+1)%4; //Update direction robot is facing
+    cd = (cd+1)%4; //Update direction robot is facing
   }
 
-    stop(robot);
+  stop(robot);
 }
 
 
@@ -336,15 +336,15 @@ void approachWall(Robot R){
 
 void correctCourse(Robot R){
   if((lVal < thresh) && (mVal > thresh) && (rVal < thresh)){ //SET MOTORS TO DRIVE FORWARD
-      analogWrite(L_Speed, R.L_MedMotorSpeed);
-      analogWrite(R_Speed, R.R_MedMotorSpeed);
-    }else if((lVal > thresh) && (mVal < thresh) && (rVal < thresh)){//LEANING INTO THE RIGHT...SPEED UP RIGHT MOTOR (CALIBRATE)
-      analogWrite(L_Speed, R.L_SlowMotorSpeed);
-      analogWrite(R_Speed, R.R_MedMotorSpeed);
-    }else if((lVal < thresh) && (mVal < thresh) && (rVal > thresh)){//LEANING INTO THE LEFT...SPEED UP RIGHT MOTOR (CALIBRATE)
-      analogWrite(L_Speed, R.L_MedMotorSpeed);
-      analogWrite(R_Speed, R.L_SlowMotorSpeed);
-    }
+    analogWrite(L_Speed, R.L_MedMotorSpeed);
+    analogWrite(R_Speed, R.R_MedMotorSpeed);
+  }else if((lVal > thresh) && (mVal < thresh) && (rVal < thresh)){//LEANING INTO THE RIGHT...SPEED UP RIGHT MOTOR (CALIBRATE)
+    analogWrite(L_Speed, R.L_SlowMotorSpeed);
+    analogWrite(R_Speed, R.R_MedMotorSpeed);
+  }else if((lVal < thresh) && (mVal < thresh) && (rVal > thresh)){//LEANING INTO THE LEFT...SPEED UP RIGHT MOTOR (CALIBRATE)
+    analogWrite(L_Speed, R.L_MedMotorSpeed);
+    analogWrite(R_Speed, R.L_SlowMotorSpeed);
+  }
 }
 
 
